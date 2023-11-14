cfed44c295bcccb9f12bb505f1c29faef3469bd7
Dmytro Firsov <dmytro_firsov@epam.com>
2023-11-14 21:25:04 +0200

    rcar3-xen: set proper env vars for FDT and initrd
    Set initrd_high and fdt_high environment variables to max value, which
    allows U-boot to place initramdisk and FDT anywhere. Previously, their
    location sometimes overlaped with reserved memory regions, which leaded
    to errors.
    
    Signed-off-by: Dmytro Firsov <dmytro_firsov@epam.com>
    Acked-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>
    Reviewed-by: Leonid Komarianskyi <leonid_komarianskyi@epam.com>

--- include/configs/rcar3-xen.h
+++ include/configs/rcar3-xen.h
@@ -5,4 +5,9 @@
 
 #define CONFIG_LNX_KRNL_IMG_TEXT_OFFSET_BASE    CONFIG_SYS_LOAD_ADDR
 
+#undef CFG_EXTRA_ENV_SETTINGS
+#define CFG_EXTRA_ENV_SETTINGS		\
+	"fdt_high=0xffffffff\0"		\
+	"initrd_high=0xffffffff\0"
+
 #endif /* __RCAR3_XEN_H */
